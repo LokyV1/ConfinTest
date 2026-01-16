f39dcca135cc9592da8a03b88831099868a020fa
LokyV1 <luigilok24@gmail.com>
2026-01-16 12:27:25 +0100

    rimossi import non utilizzati

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "./navigation-menu";
 import { cn } from "@/lib/utils";
